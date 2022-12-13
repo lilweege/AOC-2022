40970672a1fdb7be244e9e3fa35c971ffd00449e
Luigi Quattrociocchi <luigi@quattrociocchi.net>
2022-12-13 02:12:30 -0500

    Custom parser and simplify
    I don't like using eval to parse the lists

--- 13.py
+++ 13.py
@@ -1,38 +1,58 @@
-from functools import cmp_to_key
-
 FILE = "input/13.txt"
 # FILE = "sample.txt"
 f = open(FILE, "r")
-lines = [eval(l) for l in f.readlines() if l != "\n"]
-# Eval and isinstance feel like cheating but oh well
 
 
-def sign(x):
-    if x == 0: return 0
-    return abs(x) // x
+def parse_list(s):
+    # return eval(s)  # eval is cheating!
+    def parse_impl(s, i):
+        assert s[i] == '['
+        i += 1
+        lst = []
+        n = len(s)
+        while i < n:
+            c = s[i]
+            if c == '[':
+                j, inner = parse_impl(s, i)
+                lst.append(inner)
+                i = j
+            elif c == ']':
+                i += 1
+                break
+            elif c == ',':
+                i += 1
+                continue
+            else:
+                for j in range(i+1, n):
+                    if s[j] == ',' or s[j] == ']':
+                        break
+                lst.append(int(s[i:j]))
+                i = j
+        return i, lst
+    return parse_impl(s, 0)[1]
+
+lists = [parse_list(l.strip()) for l in f.readlines() if l != "\n"]
+
 
 def cmp(x, y):
     if isinstance(x, int) and isinstance(y, int):
-        return sign(y - x)
-    elif isinstance(x, list) and isinstance(y, list):
-        if any(r := cmp(a, b) for a, b in zip(x, y)):
-            return r
-        return sign(len(y) - len(x))
-    elif isinstance(x, int):
-        return cmp([x], y)
-    else:
-        return cmp(x, [y])
+        return (x > y) - (x < y)
+    if isinstance(x, int): x = [x]
+    if isinstance(y, int): y = [y]
+    if any(r := cmp(a, b) for a, b in zip(x, y)):
+        return r
+    return cmp(len(x), len(y))
 
 
 def part1():
     print(sum(idx
-        for idx, i in enumerate(range(0, len(lines), 2), 1)
-            if cmp(lines[i], lines[i+1]) == 1))
+        for idx, i in enumerate(range(0, len(lists), 2), 1)
+            if cmp(lists[i], lists[i+1]) == -1))
 
 def part2():
-    d1, d2 = [[2]], [[6]]
-    ordered = sorted(lines + [d1, d2], key=cmp_to_key(cmp), reverse=True)
-    print((ordered.index(d1) + 1) * (ordered.index(d2) + 1))
+    d1 = sum(cmp(x, [[2]]) == -1 for x in lists) + 1
+    d2 = sum(cmp(x, [[6]]) == -1 for x in lists) + 2
+    print(d1 * d2)
 
 
 part1()
