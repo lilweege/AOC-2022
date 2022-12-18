94243d5df742cfeb27d052380f666608f3bf84de
Luigi Quattrociocchi <luigi@quattrociocchi.net>
2022-12-18 14:31:56 -0500

    Optimize 18

--- 18.py
+++ 18.py
@@ -19,27 +19,30 @@ for line in lines:
     points.add((x, y, z))
 
 
-def get_sides(s):
-    yield from ((x*2+dx, y*2+dy, z*2+dz) for x, y, z in s for dx, dy, dz in dirs)
-
 def part1():
-    print(list(Counter(list(get_sides(points))).values()).count(1))
+    sides = [(x*2+dx, y*2+dy, z*2+dz) for x, y, z in points for dx, dy, dz in dirs]
+    print(list(Counter(sides).values()).count(1))
 
 
 def part2():
     vis = set()
+    sides = set()
     q = deque([(xmi, ymi, zmi)])
     while q:
         x, y, z = q.popleft()
         if not (xmi <= x <= xma and ymi <= y <= yma and zmi <= z <= zma):
             continue
+
         if (x, y, z) in vis: continue
         if (x, y, z) in points: continue
+
         vis.add((x, y, z))
         for dx, dy, dz in dirs:
             q.append((x+dx, y+dy, z+dz))
+            if ((x+dx, y+dy, z+dz)) in points:
+                sides.add((x*2+dx, y*2+dy, z*2+dz))
 
-    print(len(set(get_sides(points)) & set(get_sides(vis))))
+    print(len(sides))
 
 
 part1()
